a893b14c46c613dd25021ed1de0fed8f1acb130e
15minutOdmora <liammislej@gmail.com>
2021-06-18 11:29:13 +0200

    Added simple login.

--- app/auth.py
+++ app/auth.py
@@ -1,19 +1,23 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, session, redirect, url_for, request, flash
 from jinja2 import TemplateNotFound
 
 login = Blueprint("login", __name__, template_folder="templates")
 register = Blueprint("register", __name__, template_folder="templates")
 
 
-@login.route("/login")
+@login.route("/login", methods=["GET", "POST"])
 def show():
-    try:
+    if request.method == "POST":
+        # Get input of fields
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
+        return redirect(url_for('main_page.show'))
+    else:
         return render_template(f"auth/login.html")
-    except TemplateNotFound:
-        abort(404)
 
 
-@register.route("/register")
+@register.route("/register", methods=["GET", "POST"])
 def show():
     try:
         return render_template(f"auth/register.html")
--- app/main_app.py
+++ app/main_app.py
@@ -3,11 +3,13 @@ Main app starting point.
 
 https://flask.palletsprojects.com/en/1.1.x/quickstart
 """
+import os
 from flask import Flask
 from app.main_page import main_page
 from app.auth import login, register
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 # Blueprints
 app.register_blueprint(main_page)
